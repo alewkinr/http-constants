1da42eefd84b65054b03e8ed426b6aa2c739bace
Ramil Aleshkin <alewkin@icloud.com>
2021-02-01 09:57:07 +0300

    :bookmark: Releasing v1.0.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/alewkinr/http-constants"
 EMAIL = "alewkinr@gmail.com"
 AUTHOR = "Ramil Aleshkin"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
