ded394f4c82352812088c6041bb3280748173be8
Ramil Aleshkin <alewkin@icloud.com>
2021-02-01 10:19:10 +0300

    :white_check_mark: Fixed tests

--- tests/test_status.py
+++ tests/test_status.py
@@ -6,8 +6,8 @@ from http_constants.status import HTTPStatus
 class TestHttpStatus(TestCase):
     def test_get_code(self):
         status = HTTPStatus(200)
-        self.assert_(status.get_code(), 200)
+        self.assertTrue(status.get_code(), 200)
 
     def test_get_meaning(self):
         status = HTTPStatus(200)
-        self.assert_(status.get_meaning(), "OK")
+        self.assertTrue(status.get_meaning(), "OK")
